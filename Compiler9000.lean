a631f846e759a3e8e0d99da0a871067b6246e78e
Raito Bezarius <masterancpp@gmail.com>
2021-04-29 16:20:17 +0200

    de_bruijn: complete FVBoundness proof, start substitution

--- Compiler9000.lean
+++ Compiler9000.lean
@@ -1,7 +1,7 @@
 import Lean
 
 inductive LambdaTerm where
-| num (val : Nat)
+| var (val : Nat)
 | app (fn: LambdaTerm) (arg: LambdaTerm)
 | lambda (body: LambdaTerm)
 
@@ -10,31 +10,46 @@ def allFreeVariablesBoundBy (n: Nat) (t: LambdaTerm): Prop :=
   aux n t 0
 where
   aux n t depth : Prop := match t with
-| LambdaTerm.num (val := m) => m <= n ∨ m <= depth
+| LambdaTerm.var (val := m) => m < n + depth
 | LambdaTerm.app (fn := fn) (arg := arg) => aux n fn depth ∧ aux n arg depth
 | LambdaTerm.lambda (body := fn) => aux n fn (depth + 1)
-theorem allFreeVariablesBoundBy.auxRec (t: LambdaTerm): ∀ n d: Nat, (allFreeVariablesBoundBy.aux n t d -> allFreeVariablesBoundBy.aux (Nat.succ n) t d) := sorry
+
+theorem allFreeVariablesBoundBy.auxRec (t: LambdaTerm): ∀ n d: Nat, (allFreeVariablesBoundBy.aux n t d -> allFreeVariablesBoundBy.aux (n + 1) t d) :=
+fun n d hn => by induction t generalizing d with
+| var m => admit -- todo kek
+| app fn arg h_fn h_arg => exact ⟨ h_fn _ hn.1, h_arg _ hn.2 ⟩
+| lambda body h_body => exact h_body _ hn
 
 macro "C[" n:term "](" t:term ")" : term => `(allFreeVariablesBoundBy $n $t)
 def isClosedTerm (t: LambdaTerm): Prop := C[0](t)
 
-theorem isClosedTerm.FVBoundness (t: LambdaTerm) (h: isClosedTerm t): ∀ n: Nat, C[n](t) := by
-intro n
-induction n with
+theorem isClosedTerm.FVBoundness (t: LambdaTerm) (h: isClosedTerm t): ∀ n: Nat, C[n](t) :=
+fun n => by induction n with
 | zero => assumption
 | succ m ih => match t with
-    | LambdaTerm.num (val := p) => cases ih with
-      | inl h =>
-        apply Or.inl
-        apply Nat.leOfLt (Nat.lt_succ_of_le _)
-        assumption -- p ≤ m, therefore p ≤ m + 1.
-      | inr h => apply Or.inr; assumption -- p ≤ 0, therefore p ≤ 0.
-    | LambdaTerm.app (fn := fn) (arg := arg) => apply And.intro ; exact allFreeVariablesBoundBy.auxRec _ _ _ ih.1 ; exact allFreeVariablesBoundBy.auxRec _ _ _ ih.2
-    | LambdaTerm.lambda (body := fn) => admit -- λ ⋅ body
+    | LambdaTerm.var (val := p) => exact allFreeVariablesBoundBy.auxRec _ _ _ ih
+    | LambdaTerm.app (fn := fn) (arg := arg) => exact ⟨ allFreeVariablesBoundBy.auxRec _ _ _ ih.1, allFreeVariablesBoundBy.auxRec _ _ _ ih.2 ⟩
+    | LambdaTerm.lambda (body := fn) => exact allFreeVariablesBoundBy.auxRec _ _ _ ih
 
 -- Q1.3
-def substitute (t: LambdaTerm) (index: Nat) (expr: LambdaTerm): LambdaTerm := sorry
-theorem closedTermSubstitutedEqClosedTerm (t: LambdaTerm) (h: isClosedTerm t) (index: Nat) (expr: LambdaTerm): substitute t index expr = t := sorry
+def substitute (t: LambdaTerm) (index: Nat) (expr: LambdaTerm): LambdaTerm := 
+  aux index t 0
+where
+  aux i t depth : LambdaTerm := match t with
+  | LambdaTerm.var (val := m) => if i + depth = m then expr else t
+  | LambdaTerm.app fn arg => LambdaTerm.app (aux i fn depth) (aux i arg depth)
+  | LambdaTerm.lambda body => LambdaTerm.lambda (aux i body (depth + 1))
+
+theorem Nat.neOfLt {n m: Nat} (h: n < m): n ≠ m := sorry
+
+theorem substitute.idOnClosed (depth: Nat) (t: LambdaTerm) (ht: C[depth](t)) (index: Nat) (expr: LambdaTerm) (hexpr: isClosedTerm expr): substitute.aux t index expr depth = t :=
+by induction t with
+  -- ht: m < depth (en fait m < depth + 0)
+  -- On a depth ≤ depth + index
+  -- On obtient m ≠ depth + index
+| var m => have p: index + depth ≠ m := sorry; admit
+| app fn arg h_fn h_arg => admit
+| lambda body h_body => admit
 
 -- Q1.4
 def batchSubstitute (t: LambdaTerm) (startIndex: Nat) (exprs: List LambdaTerm): LambdaTerm := sorry
