c9f3d84c111612c07e320dd52a87597952d388de
Raito Bezarius <masterancpp@gmail.com>
2021-05-03 12:25:23 +0200

    nix: override lean-vscode to add vim

--- flake.nix
+++ flake.nix
@@ -1,21 +1,31 @@
 {
   description = "Compiler 9000";
 
-  inputs.lean.url = github:leanprover/lean4;
-  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.nixpkgs.url = "github:nixos/nixpkgs-unstable";
+  inputs.nixpkgs.follows = "lean";
+  inputs.lean.url = "github:RaitoBezarius/lean4/lean-vscode-override";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, lean, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      leanPkgs = lean.packages.${system};
-      pkg = leanPkgs.buildLeanPackage {
-        name = "Compiler9000";  # must match the name of the top-level .lean file
-        src = ./.;
-      };
-    in {
-      packages = pkg // {
-        inherit (leanPkgs) lean;
-      };
+  outputs = { self, nixpkgs, lean, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        leanPkgs = lean.packages.${system};
+        pkg = leanPkgs.buildLeanPackage.override {
+          lean-vscode = leanPkgs.lean-vscode.override {
+            vscodeExtensions = with pkgs.vscode-extensions; [ vscodevim.vim ];
+          };
+        } {
+          name =
+            "Compiler9000"; # must match the name of the top-level .lean file
+          src = ./.;
+        };
+      in {
+        packages = pkg // { inherit (leanPkgs) lean; };
 
-      defaultPackage = pkg.modRoot;
-    });
+        defaultPackage = pkg.modRoot;
+      });
 }
